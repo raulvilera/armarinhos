356c870d0bc47d2cc0e40a410113aeb8a2a725c6
raulvilera <124225576+raulvilera@users.noreply.github.com>
2026-05-17 18:37:56 -0300

    Update DashboardLayout.tsx

--- components/DashboardLayout.tsx
+++ components/DashboardLayout.tsx
@@ -3,21 +3,21 @@ import Sidebar from "./Sidebar";
 import DashboardHeader from "./DashboardHeader";
 
 interface DashboardLayoutProps {
-    children: ReactNode;
-    title: string;
-    subtitle?: string;
+  children: ReactNode;
+  title: string;
+  subtitle?: string;
 }
 
 const DashboardLayout = ({ children, title, subtitle }: DashboardLayoutProps) => {
-    return (
-        <div className="min-h-screen flex bg-background">
-            <Sidebar />
-            <main className="flex-1 ml-64 p-8">
-                <DashboardHeader title={title} subtitle={subtitle} />
-                <div>{children}</div>
-            </main>
-        </div>
-    );
+  return (
+    <div className="min-h-screen flex" style={{ background: 'hsl(40 20% 97%)' }}>
+      <Sidebar />
+      <main className="flex-1 ml-60 p-8 overflow-auto">
+        <DashboardHeader title={title} subtitle={subtitle} />
+        <div className="animate-fade-in">{children}</div>
+      </main>
+    </div>
+  );
 };
 
 export default DashboardLayout;
